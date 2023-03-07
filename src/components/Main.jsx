2801f1fa946d1c4d1d3804ff3503af6f54c33e38
Pawel Smolarski <smolenc1@gmail.com>
2023-03-07 19:03:41 +0100

    build Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
 
 const Main = () => {
   return (
@@ -8,6 +11,29 @@ const Main = () => {
           <p className="uppercase text-sm tracking-widest text-gray-600">
             Paweł Smolarski - front-end developer
           </p>
+          <h1 className="py-4 text-grey-700">
+            Hi, I'm <span className="text-[#5651e5]">Paweł</span>
+          </h1>
+          <h1 className="py-2 text-grey-700">A Front-End Web Developer</h1>
+          <p className="py-4  text-gray-600 max-w-[70%] m-auto">
+            I'm front-end web developer specializing in building exceptional
+            digital experciences. Curently, I'm focused on building responsive
+            front-end web applications while learning back-end technologies. {}
+          </p>
+          <div className="flex items-center justify-between my-4 w-full m-auto max-w-[332px]">
+            <div className="rounded-full  shadow-lg shadow-gray-400 p-5 cursor-pointer ease-in duration-500">
+              <FaLinkedin size={25} />
+            </div>
+            <div className="rounded-full  shadow-lg shadow-gray-400 p-5 cursor-pointer ease-in duration-500">
+              <FaGithub size={25} />
+            </div>
+            <div className="rounded-full  shadow-lg shadow-gray-400 p-5 cursor-pointer ease-in duration-500">
+              <AiOutlineMail size={25} />
+            </div>
+            <div className="rounded-full  shadow-lg shadow-gray-400 p-5 cursor-pointer ease-in duration-500">
+              <BsPersonLinesFill size={25} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
